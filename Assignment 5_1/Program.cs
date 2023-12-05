32f6d1d0e0b4181f29e62e0aa9c557737e208b2c
Sunil Dagur <sunildagur566@gmail.com>
2023-12-05 18:38:25 +0530

    Add project files.

---
+++ Assignment 5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+           
+            int numerator = 10;
+            int denominator = 0;
+
+            int result = DivideByZeroExample(numerator, denominator);
+
+            Console.WriteLine($"Result: {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Exception caught: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+    static int DivideByZeroExample(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+
+        return numerator / denominator;
+    }
+}
+
